913cf0231e42070ac4b841639c8280514201e73e
Benjamin Garcia <benjg208@gmail.com>
2025-12-22 11:02:51 -0600

    Add comments to Constants class for clarity on parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Constants.java
@@ -2,17 +2,20 @@ package org.firstinspires.ftc.teamcode.robot;
 
 public class Constants {
     public static class Drive {
+        // PID constants for heading lock
         public static final double HEADING_KP = 0.02;
         public static final double HEADING_KI = 0.0;
         public static final double HEADING_KD = 0.0;
     }
 
     public static class Shooter {
+        // Shooter velocity control constants
         public static final double SCALE = 670;
         public static final double INTERCEPT = 85;
         public static final double MAX_RPM = 2400;
         public static final double VELOCITY_TOLERANCE = 20;
 
+        // Feedforward gains
         public static final double kP = 20;
         public static final double kI = 0;
         public static final double kD = 0;
@@ -22,8 +25,11 @@ public class Constants {
     }
 
     public static class Vision {
+        // Camera settings
         public static final int EXPOSURE = 25;
         public static final int GAIN = -325;
+
+        // AprilTag IDs
         public static final int BLUE_GOAL_ID = 20;
         public static final int RED_GOAL_ID = 24;
     }
